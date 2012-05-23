e81fbe66355d7704a5acbe77b4d507700c2895e6
Alexandr Vronskiy <shandy.developer@gmail.com>
2012-05-23 23:51:09 +0300

    Add new AbstractModel for new EAV pattern

---
+++ module/Client/src/Client/Model/Client.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Shandy
+ * Date: 20.05.12
+ * Time: 18:46
+ * To change this template use File | Settings | File Templates.
+ */
+class Client
+{
+
+}
